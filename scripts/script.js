7e6fad758f5623a302441ce6b69c9a83f4909b9f
Rosmarin <thegreattimofey.devyatkin@gmail.com>
2024-03-13 23:43:24 +0400

    карусель

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,37 @@
-function init() {
-    let map = new ymaps.Map('map', {
-        center: [[53.503702949473485,49.39487575330353]],
-        zoom: 30,
-    })   
+// Select all slides
+const slides = document.querySelectorAll(".slide");
+
+// loop through slides and set each slides translateX property to index * 100% 
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+});
+// current slide counter
+let curSlide = 0;
+let maxSlide = slides.length - 1;
+// select next slide button
+const nextSlide = document.querySelector(".btn-next");
+
+nextSlide.addEventListener("click", function () {
+if (curSlide === maxSlide) {
+curSlide = 0;
+} else {
+curSlide ++;
 }
-ymaps.ready(init);
+slides.forEach((slide, indx) => {
+slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+});
+});
+// select prev slide button
+const prevSlide = document.querySelector(".btn-prev");
+
+prevSlide.addEventListener("click", function () {
+if (curSlide === 0) {
+curSlide = maxSlide;
+} else {
+curSlide--;
+}
+
+slides.forEach((slide, indx) => {
+slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+});
+});
